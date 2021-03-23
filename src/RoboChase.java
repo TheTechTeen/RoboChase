89d9d66fc109e1d3e9e367270f8106a5a0f54228
Aiden <k1ard@yahoo.com>
2021-03-23 18:13:56 -0400

    Initial Commit - create GUI

---
+++ src/RoboChase.java
@@ -0,0 +1,11 @@
+package robochase;
+
+public class RoboChase {
+    public static void main(String[] args) {
+        RoboGUI robotFrame = new RoboGUI(); // create ButtonFrame
+        robotFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        robotFrame.setSize(350, 400); // set frame size
+        robotFrame.setVisible(true); // display frame
+        robotFrame.setLocationRelativeTo(null);
+    }
+}
